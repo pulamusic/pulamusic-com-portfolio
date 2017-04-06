5159a6573c682978d20529342cc87e38dddafa24
Jim Carroll <jim@pulamusic.com>
2017-04-06 18:36:16 -0400

    copied css

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $(".aniBox").animate({
+            height: "toggle",
+            width: "toggle"
+        });//end of animate
+    });//end of button click
+});//end of document ready
