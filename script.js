f84695a9cd68fc8b63c65ac53257f3c21b55286e
Jim Carroll <pulamusic@users.noreply.github.com>
2018-03-15 14:11:30 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("button").click(function(){
-        $(".aniBox").animate({
-            height: "toggle",
-            width: "toggle"
-        });//end of animate
-    });//end of button click
-});//end of document ready
